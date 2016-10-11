8d374789e0ab2d42bbfb2918b86723993ff5b0cd
Arthur Lui <luiarthur@gmail.com>
2016-10-11 10:44:55 -0700

    yes

--- hw/hw1/src/Q3.jl
+++ hw/hw1/src/Q3.jl
@@ -1,11 +1,11 @@
 using RCall, MCMC
 
 R"""
-library(KMsurv)   # To get the datasets in K-M"
-library(survival) # R functions"
+library(KMsurv)   # To get the datasets in K-M
+library(survival) # R functions
 library(rcommon)  # devtools::install_github('luiarthur/rcommon')
-tongue$time <- tongue$time/10 # to obtain computational stability
 data(tongue)
+tongue$time <- tongue$time/10 # to obtain computational stability
 """;
 
 R"my.pairs(tongue)";

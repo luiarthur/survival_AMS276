d571a806eb41ccf76b10c3a0a8d924afafed0a46
Arthur Lui <luiarthur@gmail.com>
2016-10-11 18:03:28 -0700

    yes

--- hw/hw1/src/Q3.jl
+++ hw/hw1/src/Q3.jl
@@ -1,4 +1,4 @@
-using RCall, MCMC
+using RCall, MCMC, Distributions
 
 R"""
 library(KMsurv)   # To get the datasets in K-M
@@ -10,3 +10,5 @@ tongue$time <- tongue$time/10 # to obtain computational stability
 
 R"my.pairs(tongue)";
 
+aneuploid = R"tongue[which(tongue$type==1),]" 
+diploid = R"tongue[which(tongue$type==2),]" 

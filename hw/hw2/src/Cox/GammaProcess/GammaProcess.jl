b3ac4bb8dbb3e8bef1e21f44e19b83bd541ca7ee
Arthur Lui <luiarthur@gmail.com>
2016-11-07 19:40:24 -0800

    starting gamma process

---
+++ hw/hw2/src/Cox/GammaProcess/GammaProcess.jl
@@ -0,0 +1,14 @@
+module PCH # piecewise-constant hazards
+
+using Distributions, RCall
+import MCMC, Base.show
+
+export summary, plot, pch, Priorβ, Priorλ, plotsurv, est_survival
+
+include("gammaprocess.jl")
+include("summary.jl")
+include("plot.jl")
+include("plotsurv.jl")
+include("est_survival.jl")
+
+end # PCH

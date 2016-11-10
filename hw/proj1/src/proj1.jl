0569a97b8ac64bbc9a7a116ee77228f21894d5f5
Arthur Lui <luiarthur@gmail.com>
2016-11-10 13:58:40 -0800

    yes

--- hw/proj1/src/proj1.jl
+++ hw/proj1/src/proj1.jl
@@ -1,5 +1,15 @@
 using RCall
 
+#=
+model:
+# h(tᵢⱼ| wᵢ,xᵢⱼ) = h₀(tᵢⱼ) wᵢ exp(xᵢⱼ'β)
+=# 
 R"""
-dat <- read.table("dat/kidney.txt",header=TRUE,skip=5)
+library(survival)
+kidney <- read.table("dat/kidney.txt",header=TRUE,skip=5)
+kidney$sex <- ifelse(kidney$sex==2, 0, 1) # M=1, F=0
+cox_mod <- coxph(Surv(time,nu) ~ sex+age, data=kidney)
+frail_mod <- coxph(Surv(time,nu) ~ sex+age+frailty(cluster,theta=1),
+                   data=kidney)
 """
+

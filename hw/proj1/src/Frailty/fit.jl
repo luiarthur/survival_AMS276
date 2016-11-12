d78b98dcb19800d311ccf777bb12cb5617468a80
Arthur Lui <luiarthur@gmail.com>
2016-11-12 14:56:57 -0800

    yes

--- hw/proj1/src/Frailty/fit.jl
+++ hw/proj1/src/Frailty/fit.jl
@@ -31,29 +31,51 @@ end
 function fit(t::Vector{Float64}, X::Matrix{Float64}, v::Vector{Float64},
              group::Vector{Int},
              prior_β::Prior_β, prior_λ::Prior_λ,
-             prior_α::Prior_α, prior_η::Prior_η, B::Int, burn::Int;
-             printFreq::Int=0)
+             prior_α::Prior_α, prior_η::Prior_η,
+             B::Int, burn::Int; printFreq::Int=0)
 
   rig(a::Float64, b::Float64) = rand(Gamma(a, 1/b))
 
   const N = length(unique(group)) # number of groups
   const group_ind = [ find(g->g==i, group) for i in 1:N ]
   const m = length.(group_ind)
+  const S_inv = inv(prior_β.S)
 
   # precomputes
   const a_λ_new = prior_λ.a + sum(v)
 
-  function update(s::State)
-    const new_λ = rig(a_λ_new, prior_λ.b * sum(t.^s.α .* X*s.β) )
-    const new_η = rig(prior_η.a, prior_η.b + sum(m.*(s.w-log(s.w))) )
-    const new_w = [ rig(new_η + sum(v[group_ind[i]]), new_η) for i in 1:N ]
-
-    const new_β = MCMC.metropolis()
-    const new_α = MCMC.metropolis()
+  # log full-conditionals for Metropolis-step
+  function lfc_β(β::Vector{Float64},λ::Float64,α::Float64)
+    const Xb = X*β
+    return -((β-prior_β.m)'S_inv*(β-prior_β.m)/2)[1] + sum(v.*Xb-λ*t.^α.*exp(Xb))
   end
 
-  const init = State(prior_β.m,
-                     prior_λ.a/prior_λ.b
+  function lfc_α(α::Float64,λ::Float64,β::Vector{Float64})
+    if α <= 0
+      out = -Inf
+    else
+      out = prior_α.a+sum(m)*log(α) + α*(-prior_α.b+sum(v.*log(t))) -λ*sum(t.^α.*exp(X*β))
+    end
+    return out
+  end
+  
+
+  function update(s::State)
+    const new_λ = rig(a_λ_new, prior_λ.b + sum(exp(t.^s.α .* X*s.β)) )
+    println("λ: ", new_λ)
+    const new_w = [ rig(s.η + sum(v[group_ind[i]]), s.η) for i in 1:N ]
+    println("w: ", new_w)
+    const new_β = MCMC.metropolis(s.β,prior_β.cs,β->lfc_β(β,new_λ,s.α))
+    println("β: ", new_β)
+    const new_α = MCMC.metropolis(s.α,prior_α.cs,α->lfc_α(α,new_λ,new_β))
+    println("α: ", new_α)
+    const new_η = s.η # need to do this!
+    
+    return State(new_β, new_λ, new_α, new_w, new_η)
+  end
+
+  const init = State(prior_β.m, #[1.93,.00832]
+                     prior_λ.a/prior_λ.b,
                      prior_α.a/prior_α.b,
                      ones(N)*prior_η.a/prior_η.b, # prior for w vector
                      prior_η.a/prior_η.b)
--- hw/proj1/src/proj1.jl
+++ hw/proj1/src/proj1.jl
@@ -1,11 +1,13 @@
+println("Starting...")
 include("Frailty/Frailty.jl")
 using RCall
 sym(M::Matrix{Float64}) = (M' + M) / 2
+srand(276);
 
 #=
 model:
 # h(tᵢⱼ| wᵢ,xᵢⱼ) = h₀(tᵢⱼ) wᵢ exp(xᵢⱼ'β)
-=# 
+=#
 R"""
 library(survival)
 kidney <- read.table("dat/kidney.txt",header=TRUE,skip=5)
@@ -15,16 +17,19 @@ frail_mod <- coxph(Surv(time,nu) ~ sex+age+frailty(cluster,theta=1), data=kidney
 """;
 @rget kidney;
 
-const t = kidney[:time] * 1.
-const X = [kidney[:sex] kidney[:age]] * 1. # sex, age
-const v = kidney[:nu] * 1.
-const group = convert(Vector{Int64},kidney[:cluster])
+const t = kidney[:time] * 1.;
+const X = [kidney[:sex] kidney[:age]] * 1.; # sex, age
+const v = kidney[:nu] * 1.;
+const group = convert(Vector{Int64},kidney[:cluster]);
 
 prior_β = Frailty.Prior_β(zeros(2), eye(2)*10^3, sym(inv(X'X))*1.)
 prior_λ = Frailty.Prior_λ(.001,.001)
 prior_α = Frailty.Prior_α(.001,.001,.1)
 prior_η = Frailty.Prior_η(.001,.001)
 
-Frailty.fit(t,X,v,group,prior_β,prior_λ,prior_α,prior_η,1000,1000)
+println(R"frail_mod")
+out = Frailty.fit(t,X,v,group,prior_β,prior_λ,prior_α,prior_η,10,1)
 
-#println(R"frail_mod")
+#=
+include("proj1.jl")
+=#
